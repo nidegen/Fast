88c583dd61c38840239d8aa983fa1172e44b49fa
Nicolas Degen <ndegen@me.com>
2020-08-21 14:36:04 +0200

    NavigatingItem

--- Sources/Fast/View+Navigating.swift
+++ Sources/Fast/View+Navigating.swift
@@ -12,6 +12,35 @@ extension View {
   public func navigating<Destination>(isActive: Binding<Bool>, destination: Destination) -> some View where Destination: View {
     self.modifier(NavigatingModifier(isActive: isActive, destination: destination))
   }
+  
+//  public func navigating<Item, Content>(item: Binding<Item?>, @ViewBuilder content: @escaping (Item) -> Content) -> some View where Item : Identifiable, Content : View {
+//    
+//  }
+}
+
+public struct NavigatingItemModifier<Destination, Item> : ViewModifier where Destination: View, Item : Identifiable {
+  @Binding var item: Item?
+  
+  var destination: ((Item) -> Content)
+  
+  var isActive: Binding<Bool> {
+    Binding<Bool>(get: {
+      self.item != nil
+    }) { isActive in
+      self.item = isActive ? self.item : nil
+    }
+  }
+  
+  public func body(content: Content) -> some View {
+    ZStack() {
+      item.map {
+        NavigationLink(destination: destination($0), isActive: isActive) {
+          EmptyView()
+        }
+      }
+      content
+    }
+  }
 }
 
 public struct NavigatingModifier<Destination> : ViewModifier where Destination: View {
