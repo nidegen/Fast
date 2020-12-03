9ade2bc28a7b9630a5089454cbcfce34e29de635
Nicolas Degen <ndegen@me.com>
2020-12-03 14:33:49 +0100

    wip

--- Sources/Fast/Sequence+Sorted.swift
+++ Sources/Fast/Sequence+Sorted.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-extension Sequence {
+public extension Sequence {
   func sorted<T: Comparable>(by keyPath: KeyPath<Element, T>) -> [Element] {
     return sorted { a, b in
       return a[keyPath: keyPath] < b[keyPath: keyPath]
